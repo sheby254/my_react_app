c20fbbccf4638b0eb9f44ee3e8a39a47e0b7de93
shaby254 <msemakwels@gmail.com>
2026-08-19 16:32:21 +0300

    Remove Tailwind plugin dependency to pass CI pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
